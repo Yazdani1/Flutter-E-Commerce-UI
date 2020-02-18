aa6b9c5784b6dbfb31defafd27323549096076e9
Yazdani <yaz4dani@gmail.com>
2020-02-18 22:57:02 +0600

    Categories

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter_ecommerceapp_ui/pages/Cat_Shoes.dart';
 
 
 class Home extends StatefulWidget {
@@ -78,7 +79,11 @@ class _HomeState extends State<Home> {
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: <Widget>[
-                  gategories(context, shoes, "Shoes"),
+                  InkWell(child: gategories(context, shoes, "Shoes"),
+                  onTap: (){
+                    Navigator.of(context).push(_createRoute());
+                  },
+                  ),
                   gategories(context, dress, "Dress"),
                   gategories(context, oil, "Oil"),
                   gategories(context, mobile, "Mobile"),
@@ -90,7 +95,7 @@ class _HomeState extends State<Home> {
           SizedBox(height: 10.0,),
 
           Container(
-            height: 450.0,
+            height: 400.0,
             child: Card(
               elevation: 5,
               child: Column(
@@ -109,7 +114,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                   Container(
-                    height: 400.0,
+                    height: 340.0,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
@@ -125,6 +130,51 @@ class _HomeState extends State<Home> {
               ),
             ),
           ),
+
+          SizedBox(height: 10.0,),
+
+          //New items section
+
+          Container(
+            height: 400.0,
+            child: Card(
+              elevation: 5,
+              child: Column(
+                children: <Widget>[
+
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Popular New Items",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color(0xFFec5252)
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 340.0,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        newItems(context, shoes, "New", "\$80", "Ladies Shoes","10\%"),
+                        newItems(context, oil, "New", "\$20", "Brand New oil","3\%"),
+                        newItems(context, dress, "New","\$250", "Top and Shirt","15\%"),
+                        newItems(context, laptop, "New","\$250", "MacBook Pro","40\%"),
+                        newItems(context, mobile, "New","\$165", "iPhone 7+","30\%")
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+
+
+
         ],
       ),
     );
@@ -289,6 +339,159 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget newItems(BuildContext context,String image,String tag,String price,String name,String discount){
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+
+          Container(
+            child: Stack(
+              children: <Widget>[
+
+                Container(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(image,
+                      height: 150.0,
+                      width: 200.0,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 10.0,
+                  //left: 10,
+                  child: Container(
+                    height: 30.0,
+                    width: 110.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Color(0xFF37a000)
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(tag,
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          Text(name,
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black
+            ),
+          ),
+          SizedBox(height: 10.0,),
+
+          Container(
+            height: 50.0,
+            width: 200.0,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: Colors.amber
+                    ),
+                    child: Center(
+                      child: Text(price,
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(
+                  height: 10.0,
+                  indent: 6,
+                  color: Colors.black,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: Colors.green
+                    ),
+                    child: Center(
+                      child: Text(discount+" Off",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.black
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0,),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 60.0,
+              width: 200.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Color(0xFF72286f)
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("View Details",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+
+        ],
+      ),
+    );
+  }
+
+
+  //page route with animations
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => Cat_Shoes(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+//        var begin = Offset(0.0, 1.0);
+//        var end = Offset.zero;
+//        var curve = Curves.ease;
+
+        //var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(-1, 0),
+            end: Offset.zero,
+          ).animate(animation),
+          child: child,
+        );
+      },
+    );
+  }
 
 }
 
---
+++ lib/pages/Cat_Shoes.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Cat_Shoes extends StatefulWidget {
+  @override
+  _Cat_ShoesState createState() => new _Cat_ShoesState();
+}
+
+class _Cat_ShoesState extends State<Cat_Shoes> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      appBar: AppBar(
+        title: Text("All Shoes"),
+        backgroundColor: Color(0xFF72286f),
+      ),
+
+    );
+  }
+}
+
+
